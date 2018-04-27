859575656c1a8e43c1410e893324bfed5f7e9611
Tomasz Gąsior <tomasz.gasior1990@gmail.com>
2018-04-27 17:41:32 +0200

    Initial commit

---
+++ src/ArrayListReview.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class ArrayListReview {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+		
+		numbers.add(4);
+		
+
+	}
+
+}
