c7ccfa0d0310c3ac19ad6083e81b0f7c43816789
Tomasz Gąsior <tomasz.gasior1990@gmail.com>
2018-04-28 17:19:43 +0200

    ArrayList finnished

--- src/ArrayListReview.java
+++ src/ArrayListReview.java
@@ -1,11 +1,38 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class ArrayListReview {
 
 	public static void main(String[] args) {
 		ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+		// adding
+		numbers.add(10);
+		numbers.add(100);
+		numbers.add(40);
+
+		// Retrieving
+		System.out.println(numbers.get(0));
+
+		// Loop
+		System.out.println("\nIteration #1: ");
+		for (int i = 0; i < numbers.size(); i++)
+			System.out.println(numbers.get(i));
+
+		// Removing
+		numbers.remove(numbers.size() - 1);
+
+		// this is very slow
+		numbers.remove(0);
+
+		// 2nd way
+		System.out.println("\nIteration #2: ");
+		for (Integer value : numbers)
+			System.out.println(value);
 		
-		numbers.add(4);
+		//List interface..
+		
+		List<String> values = new ArrayList<String>();
 		
 
 	}
