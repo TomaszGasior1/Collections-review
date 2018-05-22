aac9261096bfc49aceb440fc8cb58d37a1090d47
Tomasz Gąsior <tomasz.gasior1990@gmail.com>
2018-05-22 13:29:09 +0200

    Added methods
    addElements
    showElements

--- src/NaturalOrder.java
+++ src/NaturalOrder.java
@@ -1,9 +1,31 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 public class NaturalOrder {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		List<String> list = new ArrayList<String>();
+		
+		Set<String> set = new TreeSet<String>();
+		
 
+	}
+	
+	private static void addElements(Collection<String> col){
+		col.add("Joe");
+		col.add("Sue");
+		col.add("Juliet");
+		col.add("Clare");
+		col.add("Mike");
+	}
+	
+	private static void showElements(Collection<String> col){
+		for(String element: col)
+			System.out.println(element);
 	}
 
 }
