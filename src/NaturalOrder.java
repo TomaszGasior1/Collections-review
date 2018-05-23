bb3d39f4c82f032fec38237a42f65092ed8a7b20
Tomasz Gąsior <tomasz.gasior1990@gmail.com>
2018-05-23 02:48:39 +0200

    finnish

--- src/NaturalOrder.java
+++ src/NaturalOrder.java
@@ -1,30 +1,81 @@
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
-import java.util.SortedSet;
 import java.util.TreeSet;
+
+class Personn implements Comparable<Personn> {
+	private String name;
+	
+	public Personn(String name){
+		this.name = name;
+	}
+	
+	public String toString(){
+	return name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Personn other = (Personn) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	@Override
+	public int compareTo(Personn p) {
+		
+		return -name.compareTo(p.name);
+	}
+}
 
 public class NaturalOrder {
 
 	public static void main(String[] args) {
-		List<String> list = new ArrayList<String>();
+		List<Personn> list = new ArrayList<Personn>();
 		
-		Set<String> set = new TreeSet<String>();
+		Set<Personn> set = new TreeSet<Personn>();
 		
+		addElements(list);
+		addElements(set);
+		
+		Collections.sort(list);
+		
+		showElements(list);
+		System.out.println();
+		showElements(set);
 
 	}
 	
-	private static void addElements(Collection<String> col){
-		col.add("Joe");
-		col.add("Sue");
-		col.add("Juliet");
-		col.add("Clare");
-		col.add("Mike");
+	private static void addElements(Collection<Personn> col){
+		col.add(new Personn("Joe"));
+		col.add(new Personn("Sue"));
+		col.add(new Personn("Juliet"));
+		col.add(new Personn("Clare"));
+		col.add(new Personn("Mike"));
 	}
 	
-	private static void showElements(Collection<String> col){
-		for(String element: col)
+	private static void showElements(Collection<Personn> col){
+		for(Personn element: col)
 			System.out.println(element);
 	}
 
